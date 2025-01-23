61f87ed0c7fc9d3894709c254d019c736ab1cf60
Little-Motivated <164312772+Little-Motivated@users.noreply.github.com>
2025-01-23 19:19:09 +0300

    Added actix-web

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpServer, HttpResponse};
+
+async fn hello() -> HttpResponse {
+    HttpResponse::Ok().body("First!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new().route("/", web::get().to(hello))
+    })
+    .bind("192.168.0.16:18318")? // Позволяет доступ извне
+    .run()
+    .await
 }
