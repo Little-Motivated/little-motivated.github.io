3029dbcda4f78c956d523871c41564877b14c18b
Little-Motivated <164312772+Little-Motivated@users.noreply.github.com>
2025-02-22 20:58:08 +0300

    Add one more flower and heart

--- script.js
+++ script.js
@@ -1,3 +1,70 @@
+const pi = Math.PI;
+
+function drawFlower1(canvas, ctx) {
+  const centerX = canvas.width / 2;
+  const centerY = canvas.height / 2;
+
+  ctx.beginPath();
+  for (let theta = 0; theta <= Math.PI * 2; theta += 0.01) {
+    const r = Math.sin(6 * theta);
+    const x = centerX + r * Math.cos(theta) * 100;
+    const y = centerY + r * Math.sin(theta) * 100;
+
+    theta === 0 ? ctx.moveTo(x, y) : ctx.lineTo(x, y);
+  }
+  ctx.closePath();
+  ctx.fillStyle = "rgb(255, 0, 0)";
+  ctx.strokeStyle = "rgb(238, 195, 3)";
+  ctx.lineWidth = 3;
+  ctx.fill();
+  ctx.stroke();
+}
+
+function drawFlower2(canvas, ctx, a) {
+  const centerX = canvas.width / 2;
+  const centerY = canvas.height / 2;
+
+  ctx.beginPath();
+  for (let theta = 0; theta <= Math.PI * 2; theta += 0.01) {
+    const r = Math.cos(theta) * 13 - Math.sin(theta * a);
+    const x = centerX + r * Math.cos(theta) * 10;
+    const y = centerY + r * Math.sin(theta) * 10;
+
+    theta === 0 ? ctx.moveTo(x, y) : ctx.lineTo(x, y);
+  }
+  for (let theta = 0; theta <= Math.PI * 2; theta += 0.01) {
+    const r = Math.cos(theta + pi) * 13 - Math.sin((theta + pi) * a);
+    const x = centerX + r * Math.cos(theta) * 10;
+    const y = centerY + r * Math.sin(theta) * 10;
+
+    theta === 0 ? ctx.moveTo(x, y) : ctx.lineTo(x, y);
+  }
+  for (let theta = 0; theta <= Math.PI * 2; theta += 0.01) {
+    const r = Math.cos(theta + pi / 2) * 13 - Math.sin((theta + pi / 2) * a);
+    const x = centerX + r * Math.cos(theta) * 10;
+    const y = centerY + r * Math.sin(theta) * 10;
+
+    theta === 0 ? ctx.moveTo(x, y) : ctx.lineTo(x, y);
+  }
+  for (let theta = 0; theta <= Math.PI * 2; theta += 0.01) {
+    const r = Math.cos(theta - pi / 2) * 13 - Math.sin((theta - pi / 2) * a);
+    const x = centerX + r * Math.cos(theta) * 10;
+    const y = centerY + r * Math.sin(theta) * 10;
+
+    theta === 0 ? ctx.moveTo(x, y) : ctx.lineTo(x, y);
+  }
+  ctx.closePath();
+  ctx.fillStyle = "rgb(255, 0, 0)";
+  ctx.strokeStyle = "rgb(238, 195, 3)";
+  ctx.lineWidth = 3;
+  ctx.fill();
+  ctx.stroke();
+}
+
+function drawHeart(canvas, ctx) {
+  
+}
+
 function draw() {
   const canvas = document.getElementById("flower");
   if (canvas.getContext) {
@@ -7,34 +74,6 @@ function draw() {
     const centerX = canvas.width / 2;
     const centerY = canvas.height / 2;
 
-    ctx.beginPath();
-    for (let theta = 0; theta <= Math.PI * 2; theta += 0.01) {
-      const r = Math.sin(6 * theta);
-      const x = centerX + r * Math.cos(theta) * 100;
-      const y = centerY + r * Math.sin(theta) * 100;
-
-      theta === 0 ? ctx.moveTo(x, y) : ctx.lineTo(x, y);
-    }
-    ctx.closePath();
-    ctx.fillStyle = "rgb(255, 0, 0)";
-    ctx.strokeStyle = "rgb(238, 195, 3)";
-    ctx.lineWidth = 3;
-    ctx.fill();
-    ctx.stroke();
-
-    ctx.beginPath();
-    for (let theta = 0; theta <= Math.PI * 2; theta += 0.01) {
-      const r = Math.sin(6 * theta);
-      const x = centerX + r * Math.cos(theta) * 100;
-      const y = centerY + r * Math.sin(theta) * 100;
-
-      theta === 0 ? ctx.moveTo(x, y) : ctx.lineTo(x, y);
-    }
-    ctx.closePath();
-    ctx.fillStyle = "rgb(255, 0, 0)";
-    ctx.strokeStyle = "rgb(238, 195, 3)";
-    ctx.lineWidth = 3;
-    ctx.fill();
-    ctx.stroke();
+    drawFlower2(canvas, ctx, 9);
   }
 }
