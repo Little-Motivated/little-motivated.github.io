501da7837387de1ad8a9e818973ce90447b7db2f
Little-Motivated <164312772+Little-Motivated@users.noreply.github.com>
2025-02-23 17:00:06 +0300

    Change flower

--- script.js
+++ script.js
@@ -24,38 +24,42 @@ function drawFlower2(canvas, ctx, a) {
   const centerX = canvas.width / 2;
   const centerY = canvas.height / 2;
 
+  const scale = 7;
+
   ctx.beginPath();
   for (let theta = 0; theta <= Math.PI * 2; theta += 0.01) {
-    const r = Math.cos(theta) * 13 - Math.sin(theta * a);
-    const x = centerX + r * Math.cos(theta) * 10;
-    const y = centerY + r * Math.sin(theta) * 10;
+    const r = Math.cos(theta) * 13 - Math.sin(theta * a) * 5;
+    const x = centerX + r * Math.cos(theta) * scale;
+    const y = centerY + r * Math.sin(theta) * scale;
 
     theta === 0 ? ctx.moveTo(x, y) : ctx.lineTo(x, y);
   }
   for (let theta = 0; theta <= Math.PI * 2; theta += 0.01) {
-    const r = Math.cos(theta + pi) * 13 - Math.sin((theta + pi) * a);
-    const x = centerX + r * Math.cos(theta) * 10;
-    const y = centerY + r * Math.sin(theta) * 10;
+    const r = Math.cos(theta + pi) * 13 - Math.sin((theta + pi) * a) * 5;
+    const x = centerX + r * Math.cos(theta) * scale;
+    const y = centerY + r * Math.sin(theta) * scale;
 
     theta === 0 ? ctx.moveTo(x, y) : ctx.lineTo(x, y);
   }
   for (let theta = 0; theta <= Math.PI * 2; theta += 0.01) {
-    const r = Math.cos(theta + pi / 2) * 13 - Math.sin((theta + pi / 2) * a);
-    const x = centerX + r * Math.cos(theta) * 10;
-    const y = centerY + r * Math.sin(theta) * 10;
+    const r =
+      Math.cos(theta + pi / 2) * 13 - Math.sin((theta + pi / 2) * a) * 5;
+    const x = centerX + r * Math.cos(theta) * scale;
+    const y = centerY + r * Math.sin(theta) * scale;
 
     theta === 0 ? ctx.moveTo(x, y) : ctx.lineTo(x, y);
   }
   for (let theta = 0; theta <= Math.PI * 2; theta += 0.01) {
-    const r = Math.cos(theta - pi / 2) * 13 - Math.sin((theta - pi / 2) * a);
-    const x = centerX + r * Math.cos(theta) * 10;
-    const y = centerY + r * Math.sin(theta) * 10;
+    const r =
+      Math.cos(theta - pi / 2) * 13 - Math.sin((theta - pi / 2) * a) * 5;
+    const x = centerX + r * Math.cos(theta) * scale;
+    const y = centerY + r * Math.sin(theta) * scale;
 
     theta === 0 ? ctx.moveTo(x, y) : ctx.lineTo(x, y);
   }
   ctx.closePath();
   ctx.fillStyle = "rgb(255, 0, 0)";
-  ctx.strokeStyle = "rgb(238, 195, 3)";
+  ctx.strokeStyle = "rgb(255, 174, 0)";
   ctx.lineWidth = 3;
   ctx.fill();
   ctx.stroke();
@@ -72,6 +76,6 @@ function draw() {
     const centerX = canvas.width / 2;
     const centerY = canvas.height / 2;
 
-    drawFlower2(canvas, ctx, 9);
+    drawFlower2(canvas, ctx, 10);
   }
 }
